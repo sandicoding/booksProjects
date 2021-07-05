3a92c9f08b4500d5dbe82da73a49ae5ca751aed8
sandi pratama <sandicoding@gmail.com>
2021-07-06 02:20:13 +0700

    upload

---
+++ src/main/java/com/example/springbookscrudapp/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.springbookscrudapp.repository;
+
+
+import com.example.springbookscrudapp.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * ''
+ */
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
